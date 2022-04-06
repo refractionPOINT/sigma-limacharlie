abb39f0a760b82da3ac767c6a20007574cc0b3e4
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2022-04-06 09:57:35 -0700

    Don't emit informational level.

--- scripts/generate_all.py
+++ scripts/generate_all.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 import sys
+import yaml
 from multiprocessing import Pool
 
 def main():
@@ -28,6 +29,17 @@ def main():
         pool.join()
 
 def processRule( sigmaDir, thisFile, outFile ):
+    # Ignore rules with certain characteristics.
+    try:
+        ruleContent = yaml.safe_load( open( thisFile, 'rb' ).read().decode() )
+        level = ruleContent.get( 'level', None )
+        if level in ( 'informational', ):
+            print( "ignoring rule %s, level: %s" % ( thisFile, level ) )
+            return
+    except Exception as e:
+        print( "failed to parse rule %s: %s" % ( thisFile, e ) )
+        return
+
     # Artifact rules.
     os.system( "python3 %stools/sigmac -t limacharlie --backend-option lc_target=artifact -c %stools/config/limacharlie.yml %s > %s" % ( sigmaDir, sigmaDir, thisFile, outFile ) )
     if os.path.getsize( outFile ) == 0:
