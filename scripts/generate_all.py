1b8d29f356bef2f580dade0606bf332ed733353c
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2023-04-04 14:36:32 -0700

    Switch to legacy repo until we figure out what the migration path is. (#13)

--- scripts/generate_all.py
+++ scripts/generate_all.py
@@ -6,8 +6,9 @@ from multiprocessing import Pool
 
 def main():
     sigmaDir = sys.argv[ 1 ]
-    rootOutputDir = sys.argv[ 2 ]
-    dirsToProcess = sys.argv[ 3 : ]
+    sigmacDir = sys.argv[ 2 ]
+    rootOutputDir = sys.argv[ 3 ]
+    dirsToProcess = sys.argv[ 4 : ]
 
     with Pool( 16 ) as pool:
         # Generate the core rules.
@@ -23,11 +24,11 @@ def main():
                         continue
                     print( "Process rule %s" % ( thisFile, ) )
                     outFile = "%s%s/%s" % ( rootOutputDir, outDir, f )
-                    pool.apply_async( processRule, ( sigmaDir, thisFile, outFile ) )
+                    pool.apply_async( processRule, ( sigmaDir, sigmacDir, thisFile, outFile ) )
         pool.close()
         pool.join()
 
-def processRule( sigmaDir, thisFile, outFile ):
+def processRule( sigmaDir, sigmacDir, thisFile, outFile ):
     # Ignore rules with certain characteristics.
     try:
         ruleContent = yaml.safe_load( open( thisFile, 'rb' ).read().decode() )
@@ -44,7 +45,7 @@ def processRule( sigmaDir, thisFile, outFile ):
         return
 
     # Artifact rules.
-    os.system( "python3 %stools/sigmac -t limacharlie --backend-option lc_target=artifact -c %stools/config/limacharlie.yml %s > %s" % ( sigmaDir, sigmaDir, thisFile, outFile ) )
+    os.system( "python3 %stools/sigmac -t limacharlie --backend-option lc_target=artifact -c %stools/config/limacharlie.yml %s > %s" % ( sigmacDir, sigmacDir, thisFile, outFile ) )
     if os.path.getsize( outFile ) == 0:
         print( "rule %s was empty, deleting" % ( outFile, ) )
         os.system( 'rm %s' % ( outFile, ) )
@@ -57,7 +58,7 @@ def processRule( sigmaDir, thisFile, outFile ):
             print( "artifact sourced rule exists, use alt-name: %s" % ( outFile, ) )
     except:
         pass
-    os.system( "python3 %stools/sigmac -t limacharlie --backend-option lc_target=edr -c %stools/config/limacharlie.yml %s > %s" % ( sigmaDir, sigmaDir, thisFile, outFile ) )
+    os.system( "python3 %stools/sigmac -t limacharlie --backend-option lc_target=edr -c %stools/config/limacharlie.yml %s > %s" % ( sigmacDir, sigmacDir, thisFile, outFile ) )
     if os.path.getsize( outFile ) == 0:
         print( "rule %s was empty, deleting" % ( outFile, ) )
         os.system( 'rm %s' % ( outFile, ) )
